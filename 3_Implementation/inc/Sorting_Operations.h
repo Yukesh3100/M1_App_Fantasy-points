71bafb647d46c4cad3cb56741843572f43195124
Yukesh3100 <94515023+Yukesh3100@users.noreply.github.com>
2021-11-24 18:52:24 +0530

    Update Sorting_Operations.h

--- 3_Implementation/inc/Sorting_Operations.h
+++ 3_Implementation/inc/Sorting_Operations.h
@@ -1,1 +1,24 @@
 
+/**
+ * @file Sorting_Operations.h
+ * @author yukesh3100
+ * @brief 
+ * @version 0.1
+ * 
+ * 
+ */
+
+
+#ifndef __SORTING_H__
+#define __SORTING_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int* variable_one, int* variable_two);
+
+void Display(int array_ptr[], int array_size);
+
+int* BubbleSort(int array_ptr[], int array_size);
+
+#endif 
