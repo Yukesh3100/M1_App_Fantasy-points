42f514cdda6f95bf34e822f17ddf49e2a9de1cb6
Yukesh3100 <94515023+Yukesh3100@users.noreply.github.com>
2021-11-25 14:43:36 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,14 @@
 
+#include "Sorting.h"
+/**
+ * @brief main function of the program
+ * 
+ */
+int main(int argc, char *argv[])
+{
+    printf("\n****Sorting****\n"); // gcc -I inc -Iunity  project_main.c src\BubbleSort.c src\Swap.c src\Display.c 
+    while(1)
+    {
+        Sorting_menu();
+    }
+}
