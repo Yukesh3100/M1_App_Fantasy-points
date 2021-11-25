aefa9572e80389f86622201db4870e456e142975
Yukesh3100 <94515023+Yukesh3100@users.noreply.github.com>
2021-11-25 09:22:45 +0530

    Update calc.c

--- 3_Implementation/src/calc.c
+++ 3_Implementation/src/calc.c
@@ -1,1 +1,12 @@
 
+#include "Sorting.h"
+/**
+ * @brief calc function 
+ * 
+
+ */
+int calc(int p1,float p2,float p3,int p4,int p5,int p6)
+{
+     float fantasy_pts= p1+p2+p3+p4+p5+p6;
+  return fantasy_pts;
+}
